b17dcf80d9b1b857122fde76a329d3f137a371ea
James Brawn, Jr <james.brawn.jr@gmail.com>
2021-03-16 18:56:43 +0800

    First pass at introducing deploy reminders.

--- src/deployToAws.ts
+++ src/deployToAws.ts
@@ -1,5 +1,9 @@
+import _ from 'lodash';
+
+import readline from 'readline';
+import fs from 'fs';
+import path from 'path';
 import * as configUtil from './util/configUtil';
-import { Promise } from 'es6-promise';
 import { writeVersionFile } from './core/writeVersionFile';
 import { updatePrivateDependencyVersions } from './core/updatePrivateDependencyVersions';
 import { updateReleaseVersion } from './core/updateReleaseVersion';
@@ -8,13 +12,13 @@ import { deployToAws } from './core/deployToAws';
 
 
 
-function main() {
+async function main() {
   const config = configUtil.getConfig();
   let envName = '';
 
   if (process.argv.length < 3) {
     console.log("Usage: node ./node_modules/metisoft-deploy-tools/dist/deployToAws.js <envName>");
-    return Promise.resolve();
+    return;
   }
 
   envName = process.argv[2];
@@ -23,24 +27,101 @@ function main() {
     throw new Error(`No settings for environment ${envName}.`);
   }
 
-  return Promise.resolve()
-    .then(() => updatePrivateDependencyVersions(config.github.apiKey, config.updatePrivateDependencyVersions.packageNames))
-    .then(() => updateReleaseVersion(config.github.repoPath, config.github.apiKey))
+  if (!await __remindToUpdateConfigFiles(envName)) {
+    return;
+  }
 
-    .then(() => {
-      if (config.writeVersionFile.destinationPath) {
-        writeVersionFile(config.writeVersionFile.destinationPath);
-      }
-    })
+  if (envName === 'prod') {
+    if (await __checkIfProdFilesAreOutOfDateComparedToDevFiles()) {
+      return;
+    }
+  }
 
-    .then(() => buildAndPushDocker(config, envName))
+  await updatePrivateDependencyVersions(config.github.apiKey, config.updatePrivateDependencyVersions.packageNames);
+  await updateReleaseVersion(config.github.repoPath, config.github.apiKey);
 
-    .then(zipFileName => deployToAws({
-      s3BucketName: config.aws.s3.bucketName,
-      s3Directory: config.aws.s3.sourceBundleDirectoryKey,
-      applicationName: envSettings.ebApplicationName,
-      environmentName: envSettings.ebEnvironmentName,
-      fileName: zipFileName
-    }));
+  if (config.writeVersionFile.destinationPath) {
+    writeVersionFile(config.writeVersionFile.destinationPath);
+  }
+
+  const zipFileName = await buildAndPushDocker(config, envName);
+
+  await deployToAws({
+    s3BucketName: config.aws.s3.bucketName,
+    s3Directory: config.aws.s3.sourceBundleDirectoryKey,
+    applicationName: envSettings.ebApplicationName,
+    environmentName: envSettings.ebEnvironmentName,
+    fileName: zipFileName
+  });
 }
 main();
+
+
+
+function __remindToUpdateConfigFiles(envName: string) {
+  return new Promise(resolve => {
+    const rl = readline.createInterface(process.stdin);
+    rl.question(`\n\nHave you already updated the config files for ${envName}? (y/n)\n\n`, answer => {
+      if (__isAffirmativeAnswer(answer)) {
+        return true;
+
+      } else {
+        console.log("Please update the config files before proceeding.");
+        return false;
+      }
+    });
+  });
+}
+
+
+
+async function __checkIfProdFilesAreOutOfDateComparedToDevFiles() {
+  // TODO replace with checks across all files for these two environments
+  const devFileName = 'dev.json';
+  const prodFileName = 'prod.json';
+
+  const devStats = fs.statSync(devFileName);
+  const prodStats = fs.statSync(prodFileName);
+
+  if (prodStats.mtimeMs < devStats.mtimeMs) {
+    return await __warnThatProdFilesAreOutOfDate();
+  }
+
+  return false;
+}
+
+
+
+function __warnThatProdFilesAreOutOfDate() {
+  return new Promise(resolve => {
+    const rl = readline.createInterface(process.stdin);
+    rl.question(
+      __genBanner("WARNING! The dev files were last modified later than the prod files. Are you sure the prod files are up to date? (y/n)"),
+      answer => {
+        if (__isAffirmativeAnswer(answer)) {
+          return true;
+
+        } else {
+          console.log("Please update the config files before proceeding.");
+          return false;
+        }
+      });
+  });
+}
+
+
+
+function __genBanner(text: string) {
+  const stars = _.repeat('*', 30);
+  return `
+${stars}\n
+${text}\n
+${stars}\n
+  `;
+}
+
+
+
+function __isAffirmativeAnswer(answer: string) {
+  return answer.trim().toLowerCase() === 'y';
+}
